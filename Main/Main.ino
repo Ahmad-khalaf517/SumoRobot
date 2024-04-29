c5358bdc6c0fd48d4b854b0fa528e49630c8f876
Ahmad Khalaf <ahmadkhalaf517@gmail.com>
2024-04-29 12:12:41 +0300

    Moving  car autonomos according to ultrasonic sensor

--- Main/Main.ino
+++ Main/Main.ino
@@ -1,5 +1,17 @@
+
 #include "Car.h"
 #include "Arduino.h"
+
+int trigPin = A8; // TRIG pin
+int echoPin = 45; // ECHO pin
+
+float duration_us, distance;
+
+// infrared to detect obstacle
+int obs_front = 41;
+int obs_back = 42;
+int obs_right = 40;
+int obs_left = 43;
 
 Car myCar;
 char autonomos = 'm';
@@ -10,6 +22,34 @@ void setup()
   myCar.setupFrontWheels(2, 3, 4, 5, 9, 10);
   myCar.setupBackWheels(7, 8, 12, 13, 6, 11);
   Serial.begin(9600);
+
+  pinMode(obs_front, INPUT);
+  pinMode(obs_back, INPUT);
+  pinMode(obs_right, INPUT);
+  pinMode(obs_left, INPUT);
+
+  // configure the trigger pin to output mode
+  pinMode(trigPin, OUTPUT);
+  // configure the echo pin to input mode
+  pinMode(echoPin, INPUT);
+}
+
+void setDistance()
+{
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(20);
+  digitalWrite(trigPin, LOW);
+
+  // measure duration of pulse from ECHO pin
+  duration_us = pulseIn(echoPin, HIGH);
+
+  // calculate the distance
+  distance = 0.017 * duration_us;
+
+  // print the value to Serial Monitor
+  Serial.print("distance: ");
+  Serial.print(distance);
+  Serial.println(" cm");
 }
 
 void stop()
@@ -43,7 +83,7 @@ void moveCar()
   stop();
 }
 
-void loop()
+void bluetooth()
 {
   if (Serial.available())
   {
@@ -66,4 +106,48 @@ void loop()
     moveCar();
   }
   delay(500);
+}
+
+void loop()
+{
+  int IR1 = digitalRead(obs_front);
+  int IR2 = digitalRead(obs_back);
+  int IR3 = digitalRead(obs_right);
+  int IR4 = digitalRead(obs_left);
+  setDistance();
+  delay(200);
+
+  if (distance > 150)
+  {
+    myCar.turnLeft();
+    setDistance();
+    delay(200);
+  }
+  else if (distance <= 90 && distance > 30)
+  {
+    myCar.forward();
+    setDistance();
+    delay(200);
+  }
+  else if (distance <= 30 && distance > 20)
+  {
+    myCar.forward();
+    setDistance();
+    delay(200);
+  }
+  IR1 = digitalRead(obs_front);
+
+  while (distance <= 20)
+  {
+    delay(500);
+
+    myCar.push();
+    setDistance();
+
+    IR1 = digitalRead(obs_front);
+    IR2 = digitalRead(obs_back);
+    IR3 = digitalRead(obs_right);
+    IR4 = digitalRead(obs_left);
+
+  }
 }
